70b69c6957c122f9aa9fd903eb877b1dbdea118c
Seweryn Kras <krasseweryn@gmail.com>
2019-09-01 23:44:01 +0200

    Tweet add image support

--- twitter_project/static/scripts/tweets/new_tweet.js
+++ twitter_project/static/scripts/tweets/new_tweet.js
@@ -4,6 +4,16 @@ $(document).ready(function () {
     var $textfield = $(".new-tweet-textarea")
     var $placeholder = $(".new-tweet-placeholder")
 
+    var $image1 = $("#new-tweet-image-1")
+    var $image2 = $("#new-tweet-image-2")
+    var $image3 = $("#new-tweet-image-3")
+    var $image4 = $("#new-tweet-image-4")
+
+    var $image1_cont = $("#new-tweet-image-cont-1")
+    var $image2_cont = $("#new-tweet-image-cont-2")
+    var $image3_cont = $("#new-tweet-image-cont-3")
+    var $image4_cont = $("#new-tweet-image-cont-4")
+
     $placeholder.text($placeholder.attr("placeholder"))
     $placeholder.click(function () {
         $textfield.focus()
@@ -51,4 +61,73 @@ $(document).ready(function () {
         updateBar(Math.floor(text_len * 100 / 256))
         twemoji.parse($textfield[0])
     }
+
+    $("#new-tweet-image-button").change(function () {
+        if (this.files) {
+            if (this.files[0])
+                slot_image(this.files[0])
+            if (this.files[1])
+                slot_image(this.files[1])
+            if (this.files[2])
+                slot_image(this.files[2])
+            if (this.files[3])
+                slot_image(this.files[3])
+        }
+    });
+
+    $(".new-tweet-media-image").click(function () {
+        $("#new-tweet-image-button").click()
+    })
+
+    function slot_image(file) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            // find the first empty img tag and fill it with the given image
+            if (!$image1.attr("src")) {
+                $image1.attr("src", e.target.result);
+                $image1_cont.removeClass("new-tweet-image-hidden");
+            } else if (!$image2.attr("src")) {
+                $image2.attr("src", e.target.result);
+                $image2_cont.removeClass("new-tweet-image-hidden");
+            } else if (!$image3.attr("src")) {
+                $image3.attr("src", e.target.result);
+                $image3_cont.removeClass("new-tweet-image-hidden");
+            } else if (!$image4.attr("src")) {
+                $image4.attr("src", e.target.result);
+                $image4_cont.removeClass("new-tweet-image-hidden");
+            }
+        }
+        reader.readAsDataURL(file)
+    }
+    $(".new-tweet-image-cont").click(function () {
+        var image = $(this).find(".new-tweet-image")
+        image.removeAttr("src")
+        $(this).addClass("new-tweet-image-hidden")
+
+        // Cascade all other images down
+
+        if (!($image1.attr("src")) && ($image2.attr("src"))) {
+            $image1.attr("src", $image2.attr("src"))
+            $image1_cont.removeClass("new-tweet-image-hidden")
+
+            $image2.removeAttr("src")
+            $image2_cont.addClass("new-tweet-image-hidden")
+        }
+        if (!$image2.attr("src") && $image3.attr("src")) {
+            $image2.attr("src", $image3.attr("src"))
+            $image2_cont.removeClass("new-tweet-image-hidden")
+
+            $image3.removeAttr("src")
+            $image3_cont.addClass("new-tweet-image-hidden")
+        }
+        if (!$image3.attr("src") && $image4.attr("src")) {
+            $image3.attr("src", $image4.attr("src"))
+            $image3_cont.removeClass("new-tweet-image-hidden")
+
+            $image4.removeAttr("src")
+            $image4_cont.addClass("new-tweet-image-hidden")
+        }
+    })
+
 });
